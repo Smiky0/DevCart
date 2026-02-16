a480036666a869da0b14bfe912dadf77bd9f80bf
Soumik <smik1172@gmail.com>
2026-02-16 20:46:46 +0530

    feat: now supports google OAuth along with github

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 // import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import prisma from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [ GitHub],
+    providers: [GitHub, Google],
     // pages: { signIn: "/signin" },
     callbacks: {
         async session({ session }) {
